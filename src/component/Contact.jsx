fc3b5ba0c4af0490bc9a718d23729b27b494c781
Jay-Kumar-Dayma <jaykhatik472@gmail.com>
2026-03-29 16:02:19 +0530

    connected contact form to production backend

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -18,7 +18,7 @@ function Contact() {
     setStatus("sending");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch('https://my-portfolio-server-r4be.onrender.com/api/contact', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
