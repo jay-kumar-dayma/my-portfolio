2d0bf61d3816110011ac334521e9685c2227f35f
Jay-Kumar-Dayma <jaykhatik472@gmail.com>
2026-03-29 14:33:04 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import About from "./component/About";
 import Resume from "./component/Resume";
 import Projects from "./component/Projects";
 import Contact from "./component/Contact";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-[#1a1a2e]">
       <Sidebar />
       <main className="flex-1 overflow-y-auto">
+        <Navbar />
         <Hero />
         <About />
         <Resume/>
